4f80cb48eb13bfc8dfd33b746dc1b0938173b234
gejian <pondcat@qq.com>
2018-09-08 13:45:51 +0800

    重载Pair

--- src/main/java/bthulu/commons/combine/Pair.java
+++ src/main/java/bthulu/commons/combine/Pair.java
@@ -5,40 +5,40 @@ import java.util.*;
 /**
  * <p>
  * 部分前端开发人员, 难以解析{@link Map}序列化后的json字符串, 他们更倾向于每个entry的key和value都有一个确定的名字,
- * 如:[{"left":1,"right":"lily"},{"left":2,"right":"lucy"}], 而不是:{1:"lily",2:"lucy"}.
+ * 如:[{"k":1,"v":"lily"},{"k":2,"v":"lucy"}], 而不是:{1:"lily",2:"lucy"}.
  * </p>
  * 因commons-lang3的{@link org.apache.commons.lang3.tuple.Pair}继承自Entry, 会导致序列化行为与map类似,
  * 故予以改写. 对需返回三个变量的, 请使用{@link org.apache.commons.lang3.tuple.Triple}.
  */
-public abstract class Pair<L, R> {
+public abstract class Pair<K, V> {
 
 	/**
 	 * <p>
 	 * Obtains an mutable keyvalue of from two objects inferring the generic types.
 	 * </p>
-	 * @param l the key element, may be null
-	 * @param r the value element, may be null
-	 * @param <L> the key element type
-	 * @param <R> the value element type
+	 * @param k the key element, may be null
+	 * @param v the value element, may be null
+	 * @param <K> the key element type
+	 * @param <V> the value element type
 	 * @return a keyvalue formed from the two parameters, not null
 	 */
-	public static <L, R> Pair<L, R> of(final L l, final R r) {
-		return new MutablePair<>(l, r);
+	public static <K, V> Pair<K, V> of(final K k, final V v) {
+		return new MutablePair<>(k, v);
 	}
 
 	/**
 	 * <p>
 	 * Obtains an keyvalue of from two objects inferring the generic types.
 	 * </p>
-	 * @param l the key element, may be null
-	 * @param r the value element, may be null
+	 * @param k the key element, may be null
+	 * @param v the value element, may be null
 	 * @param mutable is mutable
-	 * @param <L> the key element type
-	 * @param <R> the value element type
+	 * @param <K> the key element type
+	 * @param <V> the value element type
 	 * @return a keyvalue formed from the two parameters, not null
 	 */
-	public static <L, R> Pair<L, R> of(final L l, final R r, final boolean mutable) {
-		return mutable ? new MutablePair<>(l, r) : new ImmutablePair<>(l, r);
+	public static <K, V> Pair<K, V> of(final K k, final V v, final boolean mutable) {
+		return mutable ? new MutablePair<>(k, v) : new ImmutablePair<>(k, v);
 	}
 
 	/**
@@ -47,15 +47,15 @@ public abstract class Pair<L, R> {
 	 * </p>
 	 * @return the key, maybe null
 	 */
-	public abstract L getLeft();
+	public abstract K getK();
 
 	/**
 	 * <p>
 	 * Sets the key
 	 * </p>
-	 * @param l the key
+	 * @param k the key
 	 */
-	public abstract void setLeft(L l);
+	public abstract void setK(K k);
 
 	/**
 	 * <p>
@@ -63,15 +63,15 @@ public abstract class Pair<L, R> {
 	 * </p>
 	 * @return the value, maybe null
 	 */
-	public abstract R getRight();
+	public abstract V getV();
 
 	/**
 	 * <p>
 	 * Sets the value
 	 * </p>
-	 * @param r the value
+	 * @param v the value
 	 */
-	public abstract void setRight(R r);
+	public abstract void setV(V v);
 
 	/**
 	 * <p>
@@ -80,12 +80,12 @@ public abstract class Pair<L, R> {
 	 * @param map
 	 * @return
 	 */
-	public static <L, R> List<Pair<L, R>> from(Map<L, R> map) {
+	public static <K, V> List<Pair<K, V>> from(Map<K, V> map) {
 		if (map == null || map.isEmpty()) {
 			return Collections.emptyList();
 		}
-		List<Pair<L, R>> list = new ArrayList<>(map.size());
-		for (Map.Entry<L, R> entry : map.entrySet()) {
+		List<Pair<K, V>> list = new ArrayList<>(map.size());
+		for (Map.Entry<K, V> entry : map.entrySet()) {
 			list.add(of(entry.getKey(), entry.getValue()));
 		}
 		return list;
@@ -98,7 +98,7 @@ public abstract class Pair<L, R> {
 	 * @param entry
 	 * @return
 	 */
-	public static <L, R> Pair<L, R> from(Map.Entry<L, R> entry) {
+	public static <K, V> Pair<K, V> from(Map.Entry<K, V> entry) {
 		return of(entry.getKey(), entry.getValue());
 	}
 
@@ -109,13 +109,13 @@ public abstract class Pair<L, R> {
 	 * @param list
 	 * @return
 	 */
-	public static <L, R> Map<L, R> to(Collection<Pair<L, R>> list) {
+	public static <K, V> Map<K, V> to(Collection<Pair<K, V>> list) {
 		if (list == null || list.isEmpty()) {
 			return Collections.emptyMap();
 		}
-		Map<L, R> map = new HashMap<>((int) (list.size() / 0.75) + 1);
-		for (Pair<L, R> kv : list) {
-			map.put(kv.getLeft(), kv.getRight());
+		Map<K, V> map = new HashMap<>((int) (list.size() / 0.75) + 1);
+		for (Pair<K, V> kv : list) {
+			map.put(kv.getK(), kv.getV());
 		}
 		return map;
 	}
@@ -132,37 +132,37 @@ public abstract class Pair<L, R> {
 	 */
 	@Override
 	public String toString() {
-		return getLeft() + "=" + getRight();
+		return getK() + "=" + getV();
 	}
 
-	public static final class ImmutablePair<L, R> extends Pair<L, R> {
+	public static final class ImmutablePair<K, V> extends Pair<K, V> {
 
-		private final L left;
+		private final K k;
 
-		private final R right;
+		private final V v;
 
-		public ImmutablePair(L l, R r) {
-			this.left = l;
-			this.right = r;
+		public ImmutablePair(K k, V v) {
+			this.k = k;
+			this.v = v;
 		}
 
 		@Override
-		public L getLeft() {
-			return left;
+		public K getK() {
+			return k;
 		}
 
 		@Override
-		public void setLeft(L l) {
+		public void setK(K k) {
 			throw new UnsupportedOperationException();
 		}
 
 		@Override
-		public R getRight() {
-			return right;
+		public V getV() {
+			return v;
 		}
 
 		@Override
-		public void setRight(R r) {
+		public void setV(V v) {
 			throw new UnsupportedOperationException();
 		}
 
@@ -173,51 +173,51 @@ public abstract class Pair<L, R> {
 			}
 			if (o instanceof ImmutablePair) {
 				ImmutablePair that = (ImmutablePair) o;
-				return Objects.equals(left, that.left)
-						&& Objects.equals(right, that.right);
+				return Objects.equals(k, that.k)
+						&& Objects.equals(v, that.v);
 			}
 			return false;
 		}
 
 		@Override
 		public int hashCode() {
-			return Objects.hash(left, right);
+			return Objects.hash(k, v);
 		}
 
 	}
 
-	public static final class MutablePair<L, R> extends Pair<L, R> {
+	public static final class MutablePair<K, V> extends Pair<K, V> {
 
-		private L left;
+		private K k;
 
-		private R right;
+		private V v;
 
 		public MutablePair() {
 		}
 
-		public MutablePair(L l, R r) {
-			this.left = l;
-			this.right = r;
+		public MutablePair(K k, V v) {
+			this.k = k;
+			this.v = v;
 		}
 
 		@Override
-		public L getLeft() {
-			return left;
+		public K getK() {
+			return k;
 		}
 
 		@Override
-		public void setLeft(L l) {
-			this.left = l;
+		public void setK(K k) {
+			this.k = k;
 		}
 
 		@Override
-		public R getRight() {
-			return right;
+		public V getV() {
+			return v;
 		}
 
 		@Override
-		public void setRight(R r) {
-			this.right = r;
+		public void setV(V v) {
+			this.v = v;
 		}
 
 		@Override
@@ -227,15 +227,15 @@ public abstract class Pair<L, R> {
 			}
 			if (o instanceof MutablePair) {
 				MutablePair that = (MutablePair) o;
-				return Objects.equals(left, that.left)
-						&& Objects.equals(right, that.right);
+				return Objects.equals(k, that.k)
+						&& Objects.equals(v, that.v);
 			}
 			return false;
 		}
 
 		@Override
 		public int hashCode() {
-			return Objects.hash(left, right);
+			return Objects.hash(k, v);
 		}
 
 	}
